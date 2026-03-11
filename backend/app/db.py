fcd9d1a5c038db146735caec69272160123ebc47
Haku4060 <hakuhofujiwara0924@gmail.com>
2026-03-10 22:26:02 -0700

    Add temporary dataset for 2nd simualation and update https request

---
+++ backend/app/db.py
@@ -0,0 +1,22 @@
+db = {
+        1000:{
+         "user_name":"Haku",
+         "status": "offline",
+        },
+        1001:{
+         "user_name":"Ira",
+         "status": "offline",
+        },
+        1002:{
+         "user_name":"Bohr",
+         "status": "busy",
+        },
+        1003:{
+         "user_name":"Lorentz",
+         "status": "online",
+        },
+        1004:{
+         "user_name":"Max",
+         "status": "busy",
+        },
+}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
+from .db import db
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-
-def read_root():
-    return {"Hello": "World"}
+@app.get("/user/{id}")
+def get_user_info(id: int):
+    user = db[id]
+    return {"name": user["user_name"], "status":user["status"]}
