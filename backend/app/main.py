7c2de56765949afe0d478832b9cf5ca46fd24290
Iryna Kovalenko <irinakova96@gmail.com>
2026-03-25 15:15:00 -0700

    Add simulation endpoint to local server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from .db import db
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
 
 @app.get("/user/{id}")
 def get_user_info(id: int):
